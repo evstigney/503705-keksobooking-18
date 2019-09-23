3994fb4078361d28f8c596c9fe19570d0421825e
Vasilisa <evstigney2010@gmail.com>
2019-09-23 11:50:41 +0300

    выполнила задание с карточкой и подробностями

--- js/main.js
+++ js/main.js
@@ -10,17 +10,20 @@ var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
 var ADDRESS_MIN = 100;
 var ADDRESS_MAX = 999;
-var PRICE_MIN = 100;
-var PRICE_MAX = 10000;
+var PRICE_MIN = 1000;
+var PRICE_MAX = 100000;
 var ROOMS_MIN = 1;
 var ROOMS_MAX = 10;
 var GUESTS_MIN = 1;
 var GUESTS_MAX = 50;
 
 var map = document.querySelector('.map');
+var filters = document.querySelector('.map__filters-container');
 var mapPins = document.querySelector('.map__pins');
-var mapPinsFragment = document.createDocumentFragment();
 var mapPin = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapCard = document.querySelector('#card').content.querySelector('.map__card');
+var mapPinsFragment = document.createDocumentFragment();
+var mapCardsFragment = document.createDocumentFragment();
 
 var getRandomNumber = function (minNumber, maxNumber) {
   return Math.floor(Math.random() * (Math.floor(maxNumber) - Math.ceil(minNumber) + 1) + Math.ceil(minNumber));
@@ -73,7 +76,7 @@ var getOfferInfo = function () {
     'checkin': getRandomValue(CHECKIN_TIMES_ARR),
     'checkout': getRandomValue(CHECKOUT_TIMES_ARR),
     'features': getRandomArr(FEATURES_ARR),
-    'description': 'Строка с описанием. Строка с описанием. Строка с описанием.',
+    'description': 'С точки зрения банальной эрудиции каждый индивидуум не может игнорировать критерии утопического субъективизма.',
     'photos': getRandomArr(PHOTOS_ARR)
   };
   return offer;
@@ -113,18 +116,91 @@ var renderPin = function (ad) {
   return pinElement;
 };
 
+var getPopupTypeString = function (type) {
+  var typeString = 'Непонятный тип жилья';
+  if (type === 'flat') {
+    typeString = 'Квартира';
+  } else if (type === 'bungalo') {
+    typeString = 'Бунгало';
+  } else if (type === 'house') {
+    typeString = 'Дом';
+  } else {
+    typeString = 'Дворец';
+  }
+  return typeString;
+};
+
+var getPopupFeatures = function (elem, arr) {
+  var features = elem.querySelectorAll('.popup__feature');
+  var i = features.length;
+  while (i > arr.length) {
+    i--;
+    features.item([i]).remove();
+  }
+  return features;
+};
+
+var getPopupPhotos = function (elem, arr) {
+  var photo = elem.querySelector('img');
+  var i = 0;
+  photo.src = arr[i];
+  while (i < arr.length - 1) {
+    i++;
+    var photoNext = photo.cloneNode(true);
+    photoNext.src = arr[i];
+    elem.appendChild(photoNext);
+  }
+  return elem;
+};
+
+var renderCard = function (ad) {
+  var cardElement = mapCard.cloneNode(true);
+  var title = cardElement.querySelector('.popup__title');
+  var address = cardElement.querySelector('.popup__text--address');
+  var price = cardElement.querySelector('.popup__text--price');
+  var popupType = cardElement.querySelector('.popup__type');
+  var popupTextCapacity = cardElement.querySelector('.popup__text--capacity');
+  var popupTextTime = cardElement.querySelector('.popup__text--time');
+  var popupFeatures = cardElement.querySelector('.popup__features');
+  var popupDescription = cardElement.querySelector('.popup__description');
+  var popupPhotos = cardElement.querySelector('.popup__photos');
+  var popupAvatar = cardElement.querySelector('.popup__avatar');
+  title.textContent = ad.offer.title;
+  address.textContent = ad.offer.address;
+  price.textContent = ad.offer.price + '₽/ночь';
+  popupType.textContent = getPopupTypeString(ad.offer.type);
+  popupTextCapacity.textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
+  popupTextTime.textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  getPopupFeatures(popupFeatures, ad.offer.features);
+  popupDescription.textContent = ad.offer.description;
+  getPopupPhotos(popupPhotos, ad.offer.photos);
+  popupAvatar.src = ad.author.avatar;
+  return cardElement;
+};
+
 var toggleMapToActive = function () {
   map.classList.remove('map--faded');
 };
 
-var renderMatchingPins = function () {
-  var mockingAds = getMockingAdsArr(ADS_QUANTITY);
-  for (var i = 0; i < ADS_QUANTITY; i++) {
-    var pin = renderPin(mockingAds[i]);
+var renderMatchingPins = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var pin = renderPin(arr[i]);
     mapPinsFragment.appendChild(pin);
   }
   mapPins.appendChild(mapPinsFragment);
 };
 
-toggleMapToActive();
-renderMatchingPins();
+var renderMatchingCards = function (arr) {
+  var card = renderCard(arr[0]);
+  mapCardsFragment.appendChild(card);
+  map.insertBefore(mapCardsFragment, filters);
+};
+
+var renderMockingData = function () {
+  var mockingAdsArr = getMockingAdsArr(ADS_QUANTITY);
+  toggleMapToActive();
+  renderMatchingPins(mockingAdsArr);
+  renderMatchingCards(mockingAdsArr);
+};
+
+renderMockingData();
