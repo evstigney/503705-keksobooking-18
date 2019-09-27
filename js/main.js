9c56b402f13d6fa44bb9e59ace5417875ef28b21
Vasilisa <evstigney2010@gmail.com>
2019-09-27 18:11:12 +0300

    исправила проблему с нулевыми размерами пина

--- js/main.js
+++ js/main.js
@@ -125,7 +125,7 @@ var getMockingAdsArr = function (quantity) {
 var renderPin = function (ad) {
   var pinElement = mapPin.cloneNode(true);
   var pinElementImg = pinElement.querySelector('img');
-  pinElement.style = 'left: ' + (ad.location.x - pinElement.getBoundingClientRect().width / 2) + 'px; top: ' + (ad.location.y - pinElement.getBoundingClientRect().height) + 'px;';
+  pinElement.style = 'left: ' + (ad.location.x - pinElement.querySelector('img').style.width / 2) + 'px; top: ' + (ad.location.y - pinElement.querySelector('img').style.height) + 'px;';
   pinElementImg.src = ad.author.avatar;
   pinElementImg.alt = ad.offer.title;
   return pinElement;
